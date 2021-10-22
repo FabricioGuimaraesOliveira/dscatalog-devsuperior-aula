2ee4e29c207233deae5189eea403146dc521a318
Fabricio Oliveira <fguimaraesoliveira@gmail.com>
2021-10-21 22:28:42 -0400

    Databse seeding

--- backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscatolog.entities.Category;
 import com.devsuperior.dscatolog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
@@ -13,7 +15,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
+
         return categoryRepository.findAll();
     }
 }
